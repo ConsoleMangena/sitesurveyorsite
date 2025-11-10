676c48c90a48dc624c6e94fab3aca02605665e20
project <project@new-hostname.lan>
2025-11-10 08:35:58 +0200

    chore: redeploy - rebuild with Appwrite leaderboard env

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero";
 import Footer from "@/components/footer";
+import BestUsers from "@/components/best-users";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-dvh">
       <Hero />
+      <BestUsers />
       <Footer />
     </main>
   );
