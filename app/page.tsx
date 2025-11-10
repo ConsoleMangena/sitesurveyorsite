10e8078bf4135f22d2e864c10df0f9eb262ce38b
project <project@new-hostname.lan>
2025-11-10 09:39:06 +0200

    fix: remove leftover import of BestUsers from homepage (build error)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/hero";
 import Footer from "@/components/footer";
-import BestUsers from "@/components/best-users";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-dvh">
       <Hero />
-      <BestUsers />
       <Footer />
     </main>
   );
